edcdc53615e1dbef66461fb0dc325c87f244a94d
marwa.saieb <marwa.saieb@esprit.tn>
2021-05-11 17:30:53 +0100

    add mission comment with branch marwa

--- src/main/java/tn/esprit/spring/entities/Mission.java
+++ src/main/java/tn/esprit/spring/entities/Mission.java
@@ -16,7 +16,7 @@ public class Mission implements Serializable {
 	private static final long serialVersionUID = -5369734855993305723L;
 
 	// Auto Increment 
-	//git status
+	//test de git avec branch marwa
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
