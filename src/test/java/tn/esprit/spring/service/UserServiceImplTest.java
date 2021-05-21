b46da69868a442195add6bd9c83fdaa64755d684
marwa.saieb <marwa.saieb@esprit.tn>
2021-05-21 21:31:01 +0100

    Correction ParseInt and Test with Junit

--- src/test/java/tn/esprit/spring/service/UserServiceImplTest.java
+++ src/test/java/tn/esprit/spring/service/UserServiceImplTest.java
@@ -25,14 +25,14 @@ public class UserServiceImplTest {
 		@Autowired
 		IUserService us; 
 	
-		//@Test
-		//public void testRetrieveAllUsers() {
-		//	List<User> listUsers = us.retrieveAllUsers(); 
+		 @Test
+		public void testRetrieveAllUsers() {
+			List<User> listUsers = us.retrieveAllUsers(); 
 			// if there are 7 users in DB : 
-		//	Assert.assertEquals(9, listUsers.size());
-		//}
+		Assert.assertEquals(25, listUsers.size());
+		}
 		
-		
+	 
 		@Test
 		public void testAddUser() throws ParseException {
 			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
@@ -42,7 +42,7 @@ public class UserServiceImplTest {
 			Assert.assertEquals(u.getLastName(), userAdded.getLastName());
 		}
 	 
-		@Test
+		 @Test
 		public void testModifyUser() throws ParseException   {
 			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 			Date d = dateFormat.parse("2015-03-23");
@@ -53,15 +53,15 @@ public class UserServiceImplTest {
 	
 		@Test
 		public void testRetrieveUser() {
-			User userRetrieved = us.retrieveUser("1"); 
+			User userRetrieved = us.retrieveUser("25"); 
 			Assert.assertEquals(1L, userRetrieved.getId().longValue());
 		}
 		
-		//@Test
-		//public void testDeleteUser() {
-		//	us.deleteUser("3");
+		@Test
+		public void testDeleteUser() {
+		us.deleteUser("2");
 		//	Assert.assertNull(us.retrieveUser("3"));
-		//}
+		} 
 		
 		// 5 tests unitaires  
  
